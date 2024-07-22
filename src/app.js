70733ea5c812c3f6ab3532d0f985255742e4a476
Tomas Caggiano <tomascaggiano03@gmail.com>
2024-07-22 17:45:56 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,85 +1,60 @@
 import express from 'express';
 import productsRouter from './routes/products.router.js';
-import cartRouter from './routes/cart.router.js'
-import { __dirname, uploader } from '../utils.js'
-import handlebars from 'express-handlebars'
-import viewsRouter from './routes/views.router.js'
+import cartRouter from './routes/cart.router.js';
+import { __dirname, uploader } from '../utils.js';
+import handlebars from 'express-handlebars';
+import viewsRouter, { setupSocketIO } from './routes/views.router.js';
 import { Server } from 'socket.io';
-import ProductManager from '../archivos.js';
 
-const manager = new ProductManager
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-const PORT = process.env.PORT || 8000
-
-
+// Create HTTP server
 const httpServer = app.listen(PORT, () => {
-    console.log('Listening at port ');
+    console.log(`Listening at port ${PORT}`);
 });
 
-const socketServer = new Server(httpServer)
+// Initialize Socket.IO
+const socketServer = new Server(httpServer);
+
+// Apply socket middleware
+const socketMiddleware = (socketServer) => (req, res, next) => {
+    req.socketServer = socketServer;
+    next();
+};
+
+app.use(socketMiddleware(socketServer));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/src/public'))
+app.use(express.static(__dirname + '/src/public'));
 
 app.engine('hbs', handlebars.engine({
     extname: '.hbs'
-}))
+}));
 
-app.set('views', __dirname + '/src/views')
-app.set('view engine', 'hbs')
+app.set('views', __dirname + '/src/views');
+app.set('view engine', 'hbs');
 
-app.use('/', viewsRouter)
+// Use routers
+app.use('/', viewsRouter);
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartRouter);
 
-app.use('/api/products', productsRouter)
-
-app.use('/api/carts', cartRouter)
-
+// Handle file upload
 app.use('/subir-archivo', uploader.single('file'), (req, res) => {
     if (!req.file) {
-        return res.send('not possible')
+        return res.send('Upload failed');
     } else {
-        return res.send('upload succeded')
+        return res.send('Upload succeeded');
     }
-})
+});
 
-
+// Error handling middleware
 app.use((error, req, res, next) => {
-    console.log(error)
-    res.status(500).send('error 500')
-})
+    console.error(error);
+    res.status(500).send('Error 500');
+});
 
-
-
-
-
-
-
-let mensajes = []
-
-
-socketServer.on('connection', socket => {
-    console.log('client connected', socket.id);
-
-    (async() => {
-        const products = await manager.getProducts()
-        socket.emit('updateProducts', products)
-    })
-    socket.on('addProduct', async(product) => {
-        await manager.addProduct(product);
-        const products = await manager.getProducts();
-        socketServer.emit('updateProducts', products)
-    })
-
-
-    //chat
-
-    socket.on('mensaje', data => {
-        console.log('message data ', data)
-
-        mensajes.push(data)
-
-        socketServer.emit('mensajeLogs', mensajes)
-    })
-})
+// Set up Socket.IO connections using the setup function from views.router.js
+setupSocketIO(socketServer);
